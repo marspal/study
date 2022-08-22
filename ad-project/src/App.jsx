ee3b82448c3788f300164727b0d831072b984a33
andyxmq.xu <andyxmq.xu@huolala.cn>
2022-08-23 00:26:38 +0800

    feat: 完成eslint配置

--- ad-project/src/App.jsx
+++ ad-project/src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import "./style.scss"
+import React from 'react';
+import './style.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div style={{color: "blue"}} className="box">
+      <div style={{ color: 'blue' }} className="box">
         Hello React
         <span>I am Jack.</span>
       </div>
-    )
+    );
   }
 }
 
